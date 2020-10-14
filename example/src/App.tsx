b8ebf60265deaac9e90c736ea1478d1e8d26c693
Rithick Jaluthria <50109647+arcade-1024@users.noreply.github.com>
2020-10-14 07:56:37 +0530

    initital commit

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,10 +1,23 @@
 import React from 'react'
 
-import { ExampleComponent } from 'input'
+import { InputComponent } from 'input'
 import 'input/dist/index.css'
 
 const App = () => {
-  return <ExampleComponent text="Create React Library Example 😄" />
+  return (
+    <InputComponent
+      placeHolder='hello'
+      type='text'
+      isBorder={true}
+      isFilled={false}
+      isUnderlined={false}
+      shadow={true}
+      FontSize={20}
+      FontWeight={400}
+      showError={false}
+      pad={8}
+    />
+  )
 }
 
 export default App
--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,84 @@
 import * as React from 'react'
 import styles from './styles.module.css'
+// @ts-ignore
+import styled from 'styled-components'
 
 interface Props {
-  text: string
+  placeHolder: string
+  type: string
+  isBorder: boolean
+  isFilled: boolean
+  isUnderlined: boolean
+  shadow: boolean
+  FontSize: number
+  FontWeight: number
+  showError: boolean
+  pad: number
 }
 
-export const ExampleComponent = ({ text }: Props) => {
-  return <div className={styles.test}>Example Component: {text}</div>
+export const InputComponent = ({
+  placeHolder,
+  type,
+  isBorder,
+  isFilled,
+  isUnderlined,
+  shadow,
+  FontSize,
+  FontWeight,
+  showError,
+  pad
+}: Props) => {
+  let Input = null
+  const InpField = styled.input`
+    display: inline;
+    font-size: ${FontSize}px;
+    font-weight: ${FontWeight};
+    padding: ${pad}px;
+  `
+  const Label = styled.label`
+    padding: ${pad}px 0;
+    position: relative;
+  `;
+  isBorder
+    ? (Input = (
+        <Label className={styles.inputField}>
+          <InpField
+            type={type}
+            className={`${styles.inputBordered} ${
+              shadow ? styles.shadow : ' '
+            } ${showError ? styles.error : ' '}`}
+            required
+          />
+          {showError ? <span className={styles.spanError}>Error</span> : ' '}
+          <span className={styles.placeHolder}>{placeHolder}</span>
+        </Label>
+      ))
+    : null
+  isFilled
+    ? (Input = (
+        <label className={styles.inputField}>
+          <InpField
+            type={type}
+            className={`${styles.inputFilled} ${shadow ? styles.shadow : ' '}`}
+            required
+          />
+          <span className={styles.placeHolder}>{placeHolder}</span>
+        </label>
+      ))
+    : null
+  isUnderlined
+    ? (Input = (
+        <label className={styles.inputField}>
+          <InpField
+            type={type}
+            className={`${styles.inputUnderline} ${
+              shadow ? styles.shadow : ' '
+            }`}
+            required
+          />
+          <span className={styles.placeHolder}>{placeHolder}</span>
+        </label>
+      ))
+    : null
+  return <div>{Input}</div>
 }
